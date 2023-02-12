4340b32b6a4b300210f40e05f5c21f07cbb276cc
Chenran Wu <chenran1129@gmail.com>
2023-02-12 15:47:00 +0200

    Add XP, coins and quitting.

--- main.py
+++ main.py
@@ -4,23 +4,36 @@ import time
 
 SC_chance = 20
 fishing_speed_multiplier = 1
+XP = 0
+coins = 0
 
 
 def fish():
+    global XP, coins
     time_for_fish = 20 * fishing_speed_multiplier
     time.sleep(time_for_fish)
-    if random.randint(1, 20) != 21:  # 21 is only for testing
+    if random.randint(1, 20) != 21:
         randnum = random.randint(1, 108)
         if randnum <= 60:
-            print("You caught a cod.")
+            print("You caught a cod and sold it for 6 coins.")
+            XP += 25
+            coins += 6
         elif randnum <= 85:
-            print("You caught a salmon.")
+            print("You caught a salmon and sold it for 10 coins.")
+            XP += 35
+            coins += 10
         elif randnum <= 98:
-            print("You caught a pufferfish.")
+            print("You caught a pufferfish and sold it for 15 coins.")
+            XP += 50
+            coins += 15
         elif randnum <= 102:
-            print("You caught a clownfish.")
+            print("You caught a clownfish and sold it for 20 coins.")
+            XP += 100
+            coins += 20
         elif randnum <= 108:
-            print("You caught some clay.")
+            print("You caught some clay and sold them for 12 coins.")
+            XP += 30
+            coins += 12
 
 
 while True:
@@ -31,4 +44,13 @@ while True:
         print("""
 f - fish
 h - help
+s - stats
+q - quit
         """)
+    elif cmd == "s":
+        print(f"""
+XP: {XP}
+Coins: {coins}
+        """)
+    elif cmd == "q":
+        quit()
